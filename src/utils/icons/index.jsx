520cdcb5bf1b004151ea38f35817e14091d292c1
--global <--global>
2021-12-31 17:18:55 +0530

    :bento: added assets

--- src/utils/icons/index.jsx
+++ src/utils/icons/index.jsx
@@ -2,3 +2,5 @@ export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as Date } from './Date';
 export { default as Logo } from './Logo';
+export { default as Location } from './Location';
+export { default as Female } from './Female';
